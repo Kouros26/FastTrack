7acc299b694a40e0c71c87aab441b2f97343c2e2
Kouros26 <kouroslf@gmail.com>
2023-09-04 21:56:22 +0200

    Multiplayer working + inputs improved

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] Rigidbody rb;
     private bool buttonOne = false;
     private bool buttonTwo = false;
+    private bool buttonThree = false;
+    private bool buttonFour = false;
+
+    private bool buttonOneHeld = false;
+    private bool buttonTwoHeld = false;
+    private bool buttonThreeHeld = false;
+    private bool buttonFourHeld = false;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +26,8 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //Debug.Log("One : " + buttonOne + " Two : " + buttonTwo + " Three : " + buttonThree + " Four : " + buttonFour);
+        Debug.Log("Press : " + buttonOne + '\n' + "Hold : " + buttonOneHeld);
     }
 
     public void OnButtonOne(InputAction.CallbackContext context)
@@ -29,5 +38,35 @@ public class Player : MonoBehaviour
     public void OnButtonTwo(InputAction.CallbackContext context)
     {
         buttonTwo = context.action.triggered;
+    }
+
+    public void OnButtonThree(InputAction.CallbackContext context)
+    {
+        buttonThree = context.action.triggered;
+    }
+
+    public void OnButtonFour(InputAction.CallbackContext context)
+    {
+        buttonFour = context.action.triggered;
+    }
+
+    public void OnButtonOneHeld(InputAction.CallbackContext context)
+    {
+        buttonOneHeld = context.action.triggered;
+    }
+
+    public void OnButtonTwoHeld(InputAction.CallbackContext context)
+    {
+        buttonTwoHeld = context.action.triggered;
+    }
+
+    public void OnButtonThreeHeld(InputAction.CallbackContext context)
+    {
+        buttonThreeHeld = context.action.triggered;
+    }
+
+    public void OnButtonFourHeld(InputAction.CallbackContext context)
+    {
+        buttonFourHeld = context.action.triggered;
     }
 }
