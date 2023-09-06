7ac3719ccdc44a641fcdd5064e2b2f165c4ab6c3
Kouros26 <kouroslf@gmail.com>
2023-09-06 15:10:39 +0200

    HealthBar completed

--- Assets/Scripts/UI/GroupLife.cs
+++ Assets/Scripts/UI/GroupLife.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GroupLife : MonoBehaviour
 {
-    [SerializeField] int life;
-    [SerializeField] int maxLife;
+    [SerializeField] Slider lifeSlider;
+    [SerializeField] float lifeDecay = 0.02f;
+    //[SerializeField] float mistakesDamage = 0.1f;
+    //Combos aren't implemented yet
+
+    float life = 1.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -13,11 +18,10 @@ public class GroupLife : MonoBehaviour
         
     }
 
-    public void OnLifeChange(int deltaLife)
+    private void Update()
     {
-        if (life + deltaLife > maxLife)
-            life = maxLife;
+        life -= lifeDecay * Time.deltaTime;
 
-        life += deltaLife;
+        lifeSlider.value = life;
     }
 }
