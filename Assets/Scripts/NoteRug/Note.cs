a93fc533b0936f2c57cf82febc94a705d8078ae8
Camille GAUTHIER <al.gauthier@student.isartdigital.com>
2023-09-06 14:34:40 +0200

    Note is moving along time

--- Assets/Scripts/NoteRug/Note.cs
+++ Assets/Scripts/NoteRug/Note.cs
@@ -18,11 +18,21 @@ public enum StrokeTiming
     Stroke_null
 }
 
-
+//Couldn't this be just a struct ?
 public class Note : MonoBehaviour
 {
-   public NoteType  mType = NoteType.Note_Null;
-   public float     noteSpeed =  1;      //
-   public int       mRugTrack = -1;      //track id on the rug, from left to right, starting at 0, -1 is invalid;
-   public bool      isStroked = false;
+    public NoteType mType = NoteType.Note_Null;
+    public float    noteSpeed =  1;     //Time, in second, to reach the end of the track. Aka offset.
+    public int      mRugTrack = -1;     //track id on the rug, from left to right, starting at 0, -1 is invalid;
+    public bool     isStroked = false;
+
+    public float mStrokeAreaTime = -1;     //Timestamp when note should be in stroke area.
+    public float mSpawnTime = -1;          //Time note spawned
+    public float  mLerpTimer = 0;           //Internal lerp value.
+    public void Spawned()
+    {
+        this.mSpawnTime         = Time.time;
+        this.mLerpTimer         = mSpawnTime;
+        this.mStrokeAreaTime    = mSpawnTime + noteSpeed;
+    }
 }
